232774792e12355d5677808d33d0a0179f1f48ef
ramdaslandge99 <146620680+ramdaslandge99@users.noreply.github.com>
2023-10-03 17:41:32 +0530

    Add files via upload

---
+++ WebApp Gym/FitClub-Starter/src/components/Join/Join.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
+const Join = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="Join" id="join">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user-email"
+            placeholder="Enter Your Email Address"
+            id=""
+          />
+          <button className="btn btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
---
+++ WebApp Gym/FitClub-Starter/src/components/hero/Hero.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+  return (
+    <div className="hero">
+      <div className="blur hero-blur"></div>
+      <div className="left-h">
+        <Header />
+        {/* best ad section */}
+
+        <div className="the-best-ad">
+          <motion.div
+            initial={{ left: "238px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
+          <span>The best Fitness Club in the Town.</span>
+        </div>
+        {/* Hero Heading */}
+
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+
+          <div>
+            <span>Ideal Body</span>
+          </div>
+
+          <div>
+            <span>
+              In Here we will help you to shape and build your ideal body and
+              live up your life to <br />
+              fullest.
+            </span>
+          </div>
+        </div>
+
+        {/* Figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>Expert Coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>Members Joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>Fitness Programs</span>
+          </div>
+        </div>
+
+        {/* Hero Button */}
+
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      {/* End of Left SIde */}
+
+      {/* start of Right SIde */}
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <motion.div
+          initial={{ right: "-1rem" }}
+          whileInView={{ right: "4rem" }}
+          transition={{ ...transition }}
+          className="heart-rate"
+        >
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+
+        {/* hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={{ ...transition }}
+          src={hero_image_back}
+          alt=""
+          className="hero-image-back"
+        />
+
+        {/* Calories */}
+
+        <motion.div
+          initial={{ right: "37rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={{ ...transition, type: "tween" }}
+          className="calories"
+        >
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories burned</span>
+            <span>220 kcal</span>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
